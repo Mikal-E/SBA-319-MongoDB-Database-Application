4e993b14f849b8d61710da9e71fa83cb4a344701
Mikal-E <melamin101@gmail.com>
2026-07-09 23:01:57 -0400

    Created teamMember.js data file to hold the 5 documents requirement for each collection.

---
+++ data/teamMembers.js
@@ -0,0 +1,45 @@
+/* Requirement - Populate your application's collections with sample data illustrating the use case of the collections. You must include at least five sample documents per collection.
+3 of 3 - This one teamMember.js */
+export const teamMember = [
+
+    {
+        name: "Susan Welch",
+        role: "Junior Strategist",
+        email: "swelch@brief.io",
+        department: "Marketing (Integrated)",
+        status: "Active",
+    },
+    
+    {
+        name: "Morgan Stansfield",
+        role: "Graphic Designer",
+        email: "mstansfield@brief.io",
+        department: "Creative (Integrated)",
+        status: "Active",
+    },
+
+    {
+        name: "Daniel Osei",
+        role: "Copywriter",
+        email: "dosei@brief.io",
+        department: "Creative (Integrated)",
+        status: "Active",
+    },
+
+    {
+        name: "Priya Chandrasekaran",
+        role: "Senior Designer",
+        email: "pchandrasekaran@brief.io",
+        department: "Creative (Integrated)",
+        status: "Active",
+    },
+
+    {
+        name: "Tom Riordan",
+        role: "Account Manager",
+        email: "triordan@brief.io",
+        department: "Client Services",
+        status: "Inactive"
+    },
+
+];
